ca31c02771d8fc8a4de0e318cc0f9867a2062a72
yanfeizuo <18856856693@163.com>
2021-12-14 10:32:13 +0800

    update logo,privacy and term;only metamask wallet

--- src/routes/consents/assets/term.ts
+++ src/routes/consents/assets/term.ts
@@ -187,7 +187,7 @@ export default [
   {
     title: 'CALIFORNIA USERS AND RESIDENTS',
     contents: [
-      'If any complaint with us is not satisfactorily resolved, you can contact the Complaint Assistance Unit of the Division of Consumer Services of the California Department of Consumer Affairs in writing at 1625 North Market Blvd., Suite N 112, Sacramento, California 95834 or by telephone at (800) 952-5210 or (916) 445-1254.',
+      'If any complaint with us is not satisfactorily resolved, you can contact the Complaint Assistance Unit of the Division of Consumer Services of the California Department of Consumer Affairs in writing at 1625 North Market Blvd., Suite N 112, Sacramento, California 95834 or by emial at info@meter.io.',
     ],
   },
   {
@@ -201,7 +201,7 @@ export default [
     contents: [
       'In order to resolve a complaint regarding the Site or to receive further information regarding use of the Site, please contact us at:',
       'Decentralized Finance Labs, Inc.',
-      'meter.io',
+      'info@meter.io',
     ],
   },
 ]
