48d7c431676cf1c19cde1ab8f3831097e711f65c
Simon.Z <zhanghan.simon@gmail.com>
2022-12-02 15:27:09 -0800

    add deploy script

--- deploy-mainnet.sh
+++ deploy-mainnet.sh
@@ -4,8 +4,8 @@
 REACT_APP_NETWORK=mainnet yarn build
 
 # copy dist to mainnet S3
-aws s3 cp ./build/ s3://multisig.meter.io/ --recursive --acl public-read
+aws s3 cp ./build/ s3://multisig.meter/ --recursive --acl public-read
 
 # invalidate mainnet cloudfront cache
-aws cloudfront create-invalidation --distribution-id E4JJNGU5ZPAU7 --paths "/*"
+aws cloudfront create-invalidation --distribution-id E318GMJENMO7Z1 --paths "/*"
 
